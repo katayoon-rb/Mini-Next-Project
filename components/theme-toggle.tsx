11d04f6d890f210bf4a300b709894c3de42a0db6
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2025-12-19 20:34:50 +0330

    Fix Hydration Error on Theme Toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -2,10 +2,15 @@
 
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false)
   const { resolvedTheme, setTheme } = useTheme();
-  if (!resolvedTheme) return null;
+  
+  // eslint-disable-next-line react-hooks/set-state-in-effect
+  useEffect(() => { setMounted(true) }, []);
+  if (!mounted) return null;
 
   const isDark = resolvedTheme === "dark";
 
